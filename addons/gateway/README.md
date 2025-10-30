0107efd0c9b8fdc0a41ef56df695937d44d284f4
Rahul Medicharla <rmedicharla@gmail.com>
2025-10-29 22:26:43 -0400

    added barebones credentials manager gateway

---
+++ addons/gateway/README.md
@@ -0,0 +1,8 @@
+this is a credentials manager/api gateway
+
+registers each resource with client ID & client secrets
+
+verifies the client ID & secrets - issues oauth2 and refresh token - verifies refresh token
+
+todo// add decryption when validating clientsecret
+todo// set cors to only allow certain domains
---
+++ addons/gateway/main.go
@@ -0,0 +1,249 @@
+package main
+
+import (
+	"crypto/rsa"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
+)
+
+type AuthReq struct {
+	ClientId     string `json:"clientId"`
+	ClientSecret string `json:"clientSecret"`
+	AccessToken  string `json:"accessToken",omitempty`
+}
+
+var (
+	privateKey *rsa.PrivateKey
+	publicKey  *rsa.PublicKey
+)
+
+func issueAccessToken(req AuthReq) (*string, error) {
+
+	// create token
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+		"sub": req.ClientId,
+		"exp": time.Now().Add(time.Hour).Unix(),
+	})
+
+	accessToken, err := token.SignedString(privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &accessToken, err
+
+}
+
+func loadKeys(privateKeyPath string, publicKeyPath string) error {
+	privateKey_file, err := os.ReadFile(privateKeyPath)
+	if err != nil {
+		return err
+	}
+
+	// Parse RSA private key
+	privateKey, err = jwt.ParseRSAPrivateKeyFromPEM(privateKey_file)
+	if err != nil {
+		return err
+	}
+
+	// read RSA public key from file
+	publicKey_file, err := os.ReadFile(publicKeyPath)
+	if err != nil {
+		return err
+	}
+
+	// Parse RSA public key
+	publicKey, err = jwt.ParseRSAPublicKeyFromPEM(publicKey_file)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func parseClients(clients string) []AuthReq {
+	// parse clients of form c1,c2,c3 where cX = clientId:clientSecret
+	clientCredentials := make([]AuthReq, 0)
+
+	splitClients := strings.Split(clients, ",")
+
+	for _, c := range splitClients {
+		clientCreds := strings.Split(c, ":")
+		clientCredentials = append(clientCredentials, AuthReq{
+			ClientId:     clientCreds[0],
+			ClientSecret: clientCreds[1],
+		})
+	}
+
+	return clientCredentials
+}
+
+func validateClientCredentials(authReq AuthReq, clientCredentials []AuthReq) error {
+	for _, cred := range clientCredentials {
+		if cred.ClientId == authReq.ClientId && cred.ClientSecret == authReq.ClientSecret {
+			return nil
+		}
+	}
+	return fmt.Errorf("Client Credentials missing or invalid")
+}
+
+func verifyAccessToken(authReq AuthReq) (error, int) {
+	var claims jwt.MapClaims
+	tkn, err := jwt.ParseWithClaims(authReq.AccessToken, &claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("Invalid signing method")
+		}
+
+		return publicKey, nil
+	})
+	if err != nil {
+		return err, http.StatusBadRequest
+	}
+
+	if tkn == nil || !tkn.Valid {
+		return err, http.StatusUnauthorized
+	}
+
+	return nil, http.StatusOK
+}
+
+func main() {
+	r := gin.Default()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("env file not found")
+	}
+
+	// verify env variables
+	PORT, isSet := os.LookupEnv("PORT")
+	if !isSet {
+		panic(fmt.Errorf("PORT variable not defined"))
+	}
+
+	PRIVATE_KEY_PATH, isSet := os.LookupEnv("PRIVATE_KEY_PATH")
+	if !isSet {
+		panic(fmt.Errorf("PRIVATE_KEY_PATH variable not defined"))
+	}
+
+	PUBLIC_KEY_PATH, isSet := os.LookupEnv("PUBLIC_KEY_PATH")
+	if !isSet {
+		panic(fmt.Errorf("PUBLIC_KEY_PATH variable not defined"))
+	}
+
+	CLIENTS, isSet := os.LookupEnv("CLIENTS")
+	if !isSet {
+		panic(fmt.Errorf("CLIENTS variable not defined"))
+	}
+
+	// parse client credentials
+	clientCredentials := parseClients(CLIENTS)
+
+	// load keys
+	err = loadKeys(PRIVATE_KEY_PATH, PUBLIC_KEY_PATH)
+	if err != nil {
+		panic(err)
+	}
+
+	// cors
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST"},
+		AllowHeaders: []string{"Content-Type"},
+	}))
+
+	// health check endpoint
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "success",
+		})
+	})
+
+	// generate oauth2 & refresh token from client credentials
+	r.POST("/auth", func(c *gin.Context) {
+		var authReq AuthReq
+
+		// validate req
+		if err := c.BindJSON(&authReq); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		// validate client credentials
+		err := validateClientCredentials(authReq, clientCredentials)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		// generate token
+		accessToken, err := issueAccessToken(authReq)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		// send token back
+		c.JSON(http.StatusOK, gin.H{
+			"accessToken": *accessToken,
+		})
+
+	})
+
+	// validate oauth2 token
+	r.POST("/verify", func(c *gin.Context) {
+		var authReq AuthReq
+
+		// validate req
+		if err := c.BindJSON(&authReq); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if authReq.AccessToken == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": fmt.Errorf("AccessToken payload paramater not set"),
+			})
+			return
+		}
+
+		// validate client credentials
+		err := validateClientCredentials(authReq, clientCredentials)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		// generate token
+		err, responseCode := verifyAccessToken(authReq)
+		if err != nil {
+			c.JSON(responseCode, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		// send token back
+		c.JSON(responseCode, gin.H{})
+
+	})
+
+	http.ListenAndServe(fmt.Sprintf(":%s", PORT), r)
+}
